40394af69b4c7260822eade78e4be9f7e16e62e4
Kumar Abinash <abinash.b2@gmail.com>
2015-07-19 02:38:38 +0530

    Added code to fetch all events from database upon page load

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,4 +2,12 @@ class MapsController < ApplicationController
 	def index
 		@events = Event.all
 	end
+
+	def fetch
+		@events = Event.all
+
+		respond_to do |format|
+			format.json { render json: @events }
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :events
 
   root 'maps#index'
+
+  get 'maps' => 'maps#index'
+  post 'maps' => 'maps#fetch'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
