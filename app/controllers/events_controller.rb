8f4838dc9cabe37933f3a4a97f6d37ade932de8a
Kumar Abinash <abinash.b2@gmail.com>
2015-07-18 21:05:59 +0530

    Server now renders newly created event back as json, everything working perfectly next: adding address autocomplete and getting latlong of the address

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,7 +30,7 @@ class EventsController < ApplicationController
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
+        format.json { render json: @event }
       else
         format.html { render :new }
         format.json { render json: @event.errors, status: :unprocessable_entity }
